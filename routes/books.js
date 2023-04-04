51fde86c82ab6e9f6667cfbfe9e4d64bb0797f29
Amjad137 <mohammedamjath137@gmail.com>
2023-04-05 01:29:05 +0530

    IssuedBookCompleted

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const {getAllBooks,getSingleBookbyID} = require("../controllers/book-controller");
+const {getAllBooks,getSingleBookbyID,getIssuedBooks} = require("../controllers/book-controller");
 const { route } = require("./user");
 
 const router = express.Router();
@@ -27,6 +27,14 @@ router.get("/",getAllBooks);
 
 router.get("/:id",getSingleBookbyID);
 
+/**
+ * Route: /books/issued
+ * Method: GET
+ * Description: Get all issued books
+ * Access: Public
+ * Parameters: None
+ */
 
+router.get("/issued/allbooks",getIssuedBooks);
 
 module.exports = router;
